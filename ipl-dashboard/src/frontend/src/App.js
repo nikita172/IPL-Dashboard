a1741065c69909c604a37507f0deae50e7484f7a
Nikita rawat <nikitarwt789@gmail.com>
2022-05-19 20:20:11 +0530

    React app with Team Page

--- ipl-dashboard/src/frontend/src/App.js
+++ ipl-dashboard/src/frontend/src/App.js
@@ -1,17 +1,20 @@
 
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes,Navigate } from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/teams">
-            <TeamPage />
-          </Route>
-        </Routes>
+        <Fragment>
+          <Routes>
+            <Route exact path="/teams:teamName" element={<TeamPage />}>
+
+            </Route>
+          </Routes>
+        </Fragment>
       </Router>
 
     </div>
--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,15 +1,17 @@
 import {React, useEffect,useState} from 'react'
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard'
 import { MatchSmallCard } from '../components/MatchSmallCard'
 
 export const TeamPage=()=> {
 
     const [team, setTeam]= useState({matches: []});
+    const {teamName}=useParams();
 
     useEffect(
         ()=>{
             const fetchMatches = async()=>{
-                const response = await fetch('http://localhost:8080/');
+                const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data=await response.json();
                 setTeam(data);
             }
